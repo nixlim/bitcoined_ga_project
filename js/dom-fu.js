a5e157721ae09fee6b2cdf83f6b4b41e68afa5f2
nixlim <abdulhafid@gmail.com>
2014-01-21 20:15:11 +0000

    Trimming and commenting

--- js/dom-fu.js
+++ js/dom-fu.js
@@ -1,3 +1,4 @@
+// this is the code to break down the text into letters and words for op sequence
 $(document).ready(function() {
 				$("#os-phrases > h2").lettering('words').children("span").lettering().children("span").lettering(); 
 			});
